b3ab8dc9ca8e3b24c7db80ae3a6009577ec270c8
Christian Simbarashe <supernovaonline@outlook.com>
2021-11-30 20:35:39 +0000

    Initial Commit

---
+++ Disaster Environmental Management Trust/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Disaster Environmental Management Trust
+//
+//  Created by user207077 on 11/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Disaster Environmental Management Trust/Disaster_Environmental_Management_TrustApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disaster_Environmental_Management_TrustApp.swift
+//  Disaster Environmental Management Trust
+//
+//  Created by user207077 on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Disaster_Environmental_Management_TrustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
