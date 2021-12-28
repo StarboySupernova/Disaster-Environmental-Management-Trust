9586bb65a917008fd825596b7420fd2b2acd1310
Christian Simbarashe <supernovaonline@outlook.com>
2021-12-28 15:35:50 +0000

    Attempted to use StepperView as a modal sheet in AboutUs, didn't work out so good. Alternate plan is to use it in a ScrollView

--- Disaster Environmental Management Trust/Views/AboutUs.swift
+++ Disaster Environmental Management Trust/Views/AboutUs.swift
@@ -7,109 +7,178 @@
 
 import SwiftUI
 import StepperView
-//AboutUs is a modal sheet
+
 struct AboutUs: View {
     
-    let steps = [
-                 TextView(text:"Oveview", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Governance", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Structuring", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Our Objectives", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Vision", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Mission", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Values", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Geographical Areas of Operation", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Partnerships", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Affiliations", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Approaches", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Humanitarian Assistance", font: .system(size: 14, weight: .semibold)),
-                 TextView(text:"Supporting Smallholder Farmers", font: .system(size: 14, weight: .semibold)),
-    ]
+    let DemtData = DemtOrgData()
     
-    let indicators = [
-        StepperIndicationType.custom(Image(systemName:"eyeglasses").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"hammer.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"arrow.3.trianglepath").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"lightbulb.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"eye.square.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"figure.walk.diamond.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"gift.circle.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"globe.europe.africa.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"figure.stand.line.dotted.figure.stand").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"puzzlepiece.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"key.fill").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"figure.wave").font(.largeTitle).eraseToAnyView()),
-        StepperIndicationType.custom(Image(systemName:"heart.fill").font(.largeTitle).eraseToAnyView())
-    ]
+    @State var showSheet = false
+    @State var selectedGroup: InfoGrouping? = nil
     
-    let pitStopLineOptions = [
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black),
-        StepperLineOptions.custom(1, Color.black)
-    ]
+    @State var showLeadingGroup = false
+    @State var showLeadMiddleGroup = false
+    @State var showMiddleGroup = false
+    @State var showMiddleTrailingGroup = false
+    @State var showTrailingGroup = false
     
-    let pitStops = [
-        TextView(text:DemtAboutUsPitstops.p1).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p2).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p3).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p4).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p5).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p6).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p7).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p8).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p9).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p10).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p11).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p12).eraseToAnyView(),
-        TextView(text:DemtAboutUsPitstops.p13).eraseToAnyView(),
-    ]
+    enum InfoGrouping: CaseIterable, Identifiable {
+        var id: Int {hashValue}
+        
+        case leadingGroup
+        case leadMiddleGroup
+        case middleGroup
+        case middleTrailingGroup
+        case trailingGroup       //using CaseIterable and allCases
+    }
+    
+    
+    func infoGroups(_ contentToDisplay: InfoGrouping) -> some View {
+        
+        switch contentToDisplay {
+        case .leadingGroup:
+            return StepperView()
+                .addSteps(DemtData.leadingSteps)
+                .indicators(DemtData.leadingIndicators)
+                .addPitStops(DemtData.leadingPitStops)
+                .pitStopLineOptions(DemtData.leadingPitStopLineOptions)
+                .autoSpacing(true)
+                .padding()
+            
+        case .leadMiddleGroup:
+            return StepperView()
+                .addSteps(DemtData.leadMiddleSteps)
+                .indicators(DemtData.leadMiddleIndicators)
+                .addPitStops(DemtData.leadMiddlePitStops)
+                .pitStopLineOptions(DemtData.leadMiddlePitStopLineOptions)
+                .autoSpacing(true)
+                .padding()
+            
+        case .middleGroup:
+            return StepperView()
+                .addSteps(DemtData.middleSteps)
+                .indicators(DemtData.middleIndicators)
+                .addPitStops(DemtData.middlePitStops)
+                .pitStopLineOptions(DemtData.middlePitStopLineOptions)
+                .autoSpacing(true)
+                .padding()
+            
+        case .middleTrailingGroup:
+            return StepperView()
+                .addSteps(DemtData.middleTrailingSteps)
+                .indicators(DemtData.middleTrailingIndicators)
+                .addPitStops(DemtData.middleTrailingPitStops)
+                .pitStopLineOptions(DemtData.middleTrailingPitStopLineOptions)
+                .autoSpacing(true)
+                .padding()
+            
+        case .trailingGroup:
+            return StepperView()
+                .addSteps(DemtData.trailingSteps)
+                .indicators(DemtData.trailingIndicators)
+                .addPitStops(DemtData.trailingPitStops)
+                .pitStopLineOptions(DemtData.trailingPitStopLineOptions)
+                .autoSpacing(true)
+                .padding()
+        }
+        
+    }
+    /*
+     ZStack {
+         GradientBackground()
+         TabView(selection: $selectedTab) {
+             WelcomeView(selectedTab: $selectedTab)
+                 .tag(9)
+             ForEach(0 ..< Exercise.exercises.count){ index in
+                 ExerciseView(selectedTab: $selectedTab, index: index)
+                     .tag(index)
+             }
+         }
+         .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+     }
+
+     */
+    
+    /*
+     ScrollViewReader { scrollview in
+     ScrollView([.horizontal]) {
+         HStack(alignment: .center) {
+             ForEach(InfoGrouping.allCases){ index in
+                 HStack{
+                     infoGroups(index)
+                 }
+             }
+         }
+         
+     }
+ }
+     */
+    
     
     var body: some View {
-        ScrollView([.horizontal, .vertical]) {
-            VStack {
+        ZStack {
+            ContainerView {
                 HStack {
                     Text("Organizational Profile")
                         .font(.largeTitle)
                         .fontWeight(.black)
                         .kerning(2)
                 }
-                StepperView()
-                    .addSteps(steps)
-                    .indicators(indicators)
-                    .addPitStops(pitStops)
-                    .pitStopLineOptions(pitStopLineOptions)
-                    .autoSpacing(true)
-                    .padding()
+            } bottomContent: {
+                GeometryReader {geometry in
+                    VStack {
+                        Image("demtTeam")
+                            .resizedToFill(width: geometry.size.width, height: geometry.size.height * 0.3)
+                        navigationButton
+                    }
+                }
             }
+            .sheet(isPresented: $showSheet, onDismiss: {}, content: {
+                if let selectedGroup = selectedGroup {
+                    switch selectedGroup {
+                    case .leadingGroup :
+                        infoGroups(selectedGroup)
+                    case .leadMiddleGroup :
+                        infoGroups(selectedGroup)
+                    case .middleGroup:
+                        infoGroups(selectedGroup)
+                    case .middleTrailingGroup:
+                        infoGroups(selectedGroup)
+                    case .trailingGroup:
+                        infoGroups(selectedGroup)
+                    }
+                }
+            })
         }
-        
     }
     
-    struct DemtAboutUsPitstops {
-        static var p1 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p2 = "DEMT's highest decision-making body is the Board of Trustees which is responsible for setting organisational policies, providing guidance and direction to its secretariat as well as supervision of DEMT's activities. The second level is the secretariat which is hired by the Board to implement the organisation's programmes, projects and other activities. The secretariat is headed by the Head of Programs who is responsible for ensuring staff accountability and compliance to operational procedures & guidelines"
-        static var p3 = "DEMT maintains a strong field presence with four (4) full time staff, and at least seven (7) qualified and results oriented volunteers. The organisation's employees are drawn from the local communities and they possess an in-depttn understanding of DEMT's geographical area of operation, local languages, cultural norms, values and beliefs. The staff is responsible for the implementation of development programs in four areas namely Emergency Response, Resilience Building, Community Development and Monitoring and Evaluation"
-        static var p4 = "• Responding to natural or man­ made disasters and epidemics. • Capacity building of rural communities to establish income generating projects and run viable socio-economic development activities. • Promoting holistic environmental protection and rehabilitation in communities around Zimbabwe. • Promoting food security in rural communities through agricultural practices that include small irrigation schemes"
-        static var p5 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers'"
-        static var p6 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p7 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p8 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p9 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p10 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p11 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p12 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
-        static var p13 = "NEW PROJECT ALERT: We are proud to launch a new project called 'Promoting meaningful community involvement in the Zimbabwe National TB Program through Community Disease Committees (CDCs) to  enhance access to services and to break down barriers' "
+    var navigationButton: some View {
+        Button(action: {
+            showSheet = true
+            selectedGroup = .leadingGroup
+        }, label: {
+            Text("Next")
+                .fontWeight(.bold)
+                .padding([.leading, .trailing], 5)
+        })
+            .padding(.bottom, 10)
+            .buttonStyle(EmbossedButtonStyle())
     }
+    
+    /*
+     var historyButton: some View {
+         Button(action: {
+             showSheet = true
+             showHistory = true
+             exerciseSheet = .history
+         }, label: {
+             Text(NSLocalizedString("History", comment: "view user activity"))
+                 .fontWeight(.bold)
+                 .padding([.leading, .trailing], 5)
+         })
+             .padding(.bottom, 10)
+             .buttonStyle(EmbossedButtonStyle())
+     }
+     */
 }
 
 struct AboutUs_Previews: PreviewProvider {
